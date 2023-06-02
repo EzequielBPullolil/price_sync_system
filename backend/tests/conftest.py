96da156ea7ffd255b934ef52403b58bd03951cef
EzequielK-source <programacionezequielk@gmail.com>
2023-06-02 20:29:55 -0300

    Created fixtures for testing flask app - Added app and client test fixtures

---
+++ backend/tests/conftest.py
@@ -0,0 +1,14 @@
+from src.app import create_app
+import pytest
+
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config["TESTING"] = True
+    yield app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
