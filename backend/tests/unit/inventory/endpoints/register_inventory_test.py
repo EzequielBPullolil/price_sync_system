57341e0ab3fcea2264eba845ac1744546238ac24
EzequielK-source <programacionezequielk@gmail.com>
2023-06-03 17:54:48 -0300

    (Fix)Added requireds entry fields to all test request

--- backend/tests/unit/inventory/endpoints/register_inventory_test.py
+++ backend/tests/unit/inventory/endpoints/register_inventory_test.py
@@ -6,32 +6,35 @@ class TestRegisterInventoryEndpoint:
         - Request endpoint with barcode already registered responds with status code 400 and error information
         - Making an incomplete request to the endpoint responds with status 400 and error information
     """
+    valid_request_fields = {
+            "barcode": "nonregisteredbarcode",
+            "name": "bon o bon",
+            "price": 9000,
+            "stock": 10
+        }
 
     def test_valid_request_responds_with_status_201(self, client):
         """
           Request register inventory endpoint with valid inventory fields 
           responds with status 201 and inventory dao 
         """
-        request_body = {
-            "barcode": "abcdf"
-        }
-        response = client.post("/inventory", json=request_body)
+        response = client.post("/inventory", json=self.valid_request_fields)
 
         assert response.status_code == 201
         json_response = response.get_json()
         assert json_response["status"] == "Inventory created"
 
         inventory_data = json_response["inventory"]
-        assert inventory_data["barcode"] == request_body["barcode"]
+        assert inventory_data["barcode"] == self.valid_request_fields["barcode"]
 
     def test_request_with_barcode_already_registered_responds_with_status_400(self, client, registered_barcode):
         """
           Verify if request the endpoint with already registered barcode responds with status 400 
           and error
         """
-        response = client.post("/inventory", json={
-            "barcode": registered_barcode
-        })
+        entry_invalid_barcode = self.valid_request_fields
+        entry_invalid_barcode["barcode"] = registered_barcode
+        response = client.post("/inventory", json=entry_invalid_barcode)
 
         assert response.status_code == 400
         json_response = response.get_json()
@@ -45,14 +48,9 @@ class TestRegisterInventoryEndpoint:
 
         """
         necessary_fields = ["barcode", "name", "price", "stock"]
-        valid_request_fields = {
-            "barcode": "nonregisteredbarcode",
-            "name": "bon o bon",
-            "price": 9000,
-            "stock": 10
-        }
+        
         for field_to_delete in necessary_fields:
-            request_body = valid_request_fields
+            request_body = self.valid_request_fields
             del request_body[field_to_delete]
             response = client.post("/inventory", json=request_body)
 
