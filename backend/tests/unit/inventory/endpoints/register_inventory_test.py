2f2d832d42935b326ab6573b4333015333f8dcd1
EzequielK-source <programacionezequielk@gmail.com>
2023-06-03 13:20:59 -0300

    (test)Added testcase for register inventory endpoint - Added a testcase for verify if request register inventory endpoint with already registered barcode responds with status 400

--- backend/tests/unit/inventory/endpoints/register_inventory_test.py
+++ backend/tests/unit/inventory/endpoints/register_inventory_test.py
@@ -7,7 +7,7 @@ class TestRegisterInventoryEndpoint:
         - Making an incomplete request to the endpoint responds with status 400 and error information
     """
 
-    def test_valid_request_responds_with_satus_201(self, client):
+    def test_valid_request_responds_with_status_201(self, client):
         """
           Request register inventory endpoint with valid inventory fields 
           responds with status 201 and inventory dao 
@@ -15,3 +15,14 @@ class TestRegisterInventoryEndpoint:
         response = client.post("/inventory")
 
         assert response.status_code == 201
+
+    def test_request_with_barcode_already_registered_responds_with_status_400(self, client, registered_barcode):
+        """
+          Verify if request the endpoint with already registered barcode responds with status 400 
+          and error
+        """
+        response = client.post("/inventory", json={
+            "barcode": registered_barcode
+        })
+
+        assert response.status_code == 400
