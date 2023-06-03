ec02db914ff7fb203b848dd3547dcc6c4ccd0db0
EzequielK-source <programacionezequielk@gmail.com>
2023-06-03 14:57:40 -0300

    Added InventoryRegister class service

---
+++ backend/src/inventory/services/inventory_register.py
@@ -0,0 +1,42 @@
+from src.exceptions import AlreadyRegisteredBarcode
+from src.inventory.model import Inventory
+
+
+class InventoryRegister:
+
+    def __init__(self, db_session) -> None:
+        self.session = db_session
+
+    def register(self, inventory_data):
+        """
+            Persist and return the inventory
+            with the fields passed by parameter
+
+            Args: 
+                inventory_data(InventoryFields)
+                    - barcode(str): The Inventory barcode
+
+            Returns: InventoryDAO
+            Raises: AlreadyRegisteredBarcode
+        """
+        self.__is_barcode_avaible(inventory_data["barcode"])
+        inventory = Inventory(
+            barcode=inventory_data["barcode"]
+        )
+        self.session.add(
+            inventory
+        )
+        self.session.commit()
+
+    def __is_barcode_avaible(self, barcode):
+        """
+            Raise exception if exist an product registered
+            with the barcode pased 
+
+            Raises: AlreadyRegisteredBarcode
+        """
+        query = self.session.query(Inventory).filter_by(
+            barcode=barcode
+        ).first()
+        if (query != None):
+            raise AlreadyRegisteredBarcode(barcode)
