2aace7795735b8c36bd71a8be2547301f1cccabb
EzequielK-source <programacionezequielk@gmail.com>
2023-06-03 18:08:14 -0300

    Create integration test for register endpoint - Added testcase for verify the inventory persist - Rename unit register endpoint test file

---
+++ backend/tests/integration/inventory/endpoints/register_inventory_test.py
@@ -0,0 +1,25 @@
+from src.db import DbSession
+from src.inventory.model import Inventory
+
+
+class TestRegisterInventoryEndpointIntegration:
+    session = DbSession()
+
+    def test_valid_request_register_endpoint_persist_inventory_in_db(self, client):
+        """
+          Verify if the valid request register inventory
+          endpoint persist an inventory 
+        """
+        barcode = "integration_test_br_persist"
+        response = client.post("/inventory", json={
+            "barcode": barcode,
+            "name": "Sal",
+            "price": 9000,
+            "stock": 10
+        })
+
+        assert response.status_code == 201
+        persisted_inventory = self.session.query(
+            Inventory).filter_by(barcode=barcode).first()
+
+        assert persisted_inventory != None
