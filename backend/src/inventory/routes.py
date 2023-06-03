16e017660b7c604b807d513178265d43aeb9c2b1
EzequielK-source <programacionezequielk@gmail.com>
2023-06-03 14:49:14 -0300

    Added docstring to register_inventory endpoint

--- backend/src/inventory/routes.py
+++ backend/src/inventory/routes.py
@@ -4,4 +4,14 @@ inventory_bp = Blueprint('inventory', __name__, url_prefix='/inventory')
 
 @inventory_bp.route('/', methods=["POST"],  strict_slashes=False)
 def register_inventory():
+    """
+        Register an inventory in the database and
+        create a log after validating the request fields
+
+        :reqjson barcode: The inventory barcode
+
+        :return: Success message and persisted inventoryDAO
+
+        :rtype: json
+    """
     return {}, 201
