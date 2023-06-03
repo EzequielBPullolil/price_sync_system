cd6701eb47159fcbb7a5846284ee9fdbe34abad8
EzequielK-source <programacionezequielk@gmail.com>
2023-06-03 19:07:00 -0300

    Added columns to Inventory model - Added name, price and stock columns - Added name, price and stock params to constructor

--- backend/src/inventory/model.py
+++ backend/src/inventory/model.py
@@ -1,5 +1,5 @@
 from src.db import Base, engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Numeric
 
 
 class Inventory(Base):
@@ -7,9 +7,15 @@ class Inventory(Base):
 
     id = Column(Integer, primary_key=True)
     barcode = Column(String, unique=True)
+    name = Column(String, unique=True)
+    price = Column(Integer)
+    stock = Column(Integer)
 
-    def __init__(self, barcode):
+    def __init__(self, barcode, name, price, stock):
         self.barcode = barcode
+        self.name = name
+        self.price = price
+        self.stock = stock
 
 
 Base.metadata.create_all(engine)
