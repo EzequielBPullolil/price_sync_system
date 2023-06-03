6358391370afff27680540d410727597b787a00b
EzequielK-source <programacionezequielk@gmail.com>
2023-06-03 17:47:37 -0300

    Added Application layer exceptions - Added MissingRequiredEntry exception

--- backend/src/exceptions.py
+++ backend/src/exceptions.py
@@ -1,8 +1,26 @@
+"""
+    Here the layer exceptions and specific case are defined
+"""
+
+
 class DomainException(Exception):
     def __init__(self, message):
         super().__init__()
         self.message = message
         self.status = "error"
+
+
+class ApplicationLayerException(Exception):
+    def __init__(self, message):
+        super().__init__()
+        self.message = message
+        self.status = "error"
+
+
+class MissingRequiredEntry(ApplicationLayerException):
+    def __init__(self, missingEntry):
+        super().__init__(
+            message=f"The entry {missingEntry} is missing")
 
 
 class AlreadyRegisteredBarcode(DomainException):
