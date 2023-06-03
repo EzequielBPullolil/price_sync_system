643d26581c6350f7bd2e8c83fd86b3856acbe293
EzequielK-source <programacionezequielk@gmail.com>
2023-06-03 17:47:57 -0300

    Added global errorHandler for ApplicationLayerException

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,12 +1,21 @@
 from flask import Flask, make_response, jsonify
 from src.inventory.routes import inventory_bp
-from src.exceptions import DomainException
+from src.exceptions import ApplicationLayerException, DomainException
 
 
 def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(inventory_bp)
+
+    @app.errorhandler(ApplicationLayerException)
+    def applicatin_error_handler(error):
+        return make_response(
+            jsonify({
+                "status": error.status,
+                "message": error.message
+            }), 400
+        )
 
     @app.errorhandler(DomainException)
     def domain_error_handler(error):
@@ -17,4 +26,5 @@ def create_app():
                 "message": error.message
             }), 400
         )
+
     return app
