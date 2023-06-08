3d69921069c79a0158d884d46bdc1a03ce81d984
EzequielK-source <programacionezequielk@gmail.com>
2023-06-08 16:02:59 -0300

    (Docs) Adjusted the docstring of the create_user endpoint test - Moved the description of a valid request to the general class docstring

--- backend/tests/unit/user/endpoint/create_test.py
+++ backend/tests/unit/user/endpoint/create_test.py
@@ -13,12 +13,19 @@ class TestCreateUserEndpoint:
         Verify that sending a valid request to the 'create_user' endpoint responds
         with a status code of 201 and a valid 'user dao' object.
 
-        A valid request consists of
-            - That the user of the session has sufficient privileges to create a user, these are role master or admin 
-            - Valid reqbody
-            * name: Not registered name and length > 3
-            * password: String length > 8
-            * role: valid role id
+    A valid request consists of
+        - That the user of the session has sufficient privileges to create a user, these are role master or admin 
+        - Valid reqbody
+        * name: Not registered name and length > 3
+        * password: String length > 8
+        * role: valid role id
+   """
+
+    def test_valid_request_responds_with_status_201_and_user_dao(self, client_with_session):
+        """
+        Verify that sending a valid request to the 'create_user' endpoint responds
+        with a status code of 201 and a valid 'user dao' object.
+
         Expected behavior:
             - The response should have a status code of 201
             - The response should contain a 'user dao' object with the expected fields and values.
