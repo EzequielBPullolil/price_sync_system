5be46eb9914685197619240202f16d6094abf439
EzequielK-source <programacionezequielk@gmail.com>
2023-06-08 17:37:15 -0300

    Created User, Role and user_role tables

--- backend/src/user_role/models.py
+++ backend/src/user_role/models.py
@@ -2,8 +2,6 @@ import uuid
 from src.db import engine, Base
 from sqlalchemy import Column, String, ForeignKey, Integer
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -22,6 +20,22 @@ class User(Base):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
+class Role(Base):
+    __tablename__ = 'roles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+
+
+class RoleUser(Base):
+    __tablename__ = 'role_user'
+    user_id = Column(UUID(as_uuid=True), ForeignKey(
+        'users.id'), primary_key=True)
+    role_id = Column(Integer, ForeignKey('roles.id'), primary_key=True)
+
+    def __repr__(self):
+        return f"RoleUser(user_id={self.user_id}, role_id={self.role_id})"
 
 
 Base.metadata.create_all(engine)
