ae72fc8969440865cc72800fe4116e2caa98157d
EzequielK-source <programacionezequielk@gmail.com>
2023-06-10 17:55:53 -0300

    Refact UserDAO for show role_name

--- backend/src/user/utils.py
+++ backend/src/user/utils.py
@@ -1,13 +1,15 @@
 class UserDAO:
-    def __init__(self, user_query_response) -> None:
+    def __init__(self, user_query_response, role_name) -> None:
         """
           gets the response from an orm request and converts it to a Data access object
         """
         self.id = user_query_response.id
         self.name = user_query_response.name
+        self.role_name = role_name
 
     def to_dict(self) -> dict:
         return {
             "id": self.id,
-            "name": self.name
+            "name": self.name,
+            "role_name": self.role_name
         }
