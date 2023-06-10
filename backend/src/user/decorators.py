9bf97e6e59c64e9ee8208bd7174a662fb1a9ad18
EzequielK-source <programacionezequielk@gmail.com>
2023-06-10 19:01:54 -0300

    Created validate_create_user_fields decorators

--- backend/src/user/decorators.py
+++ backend/src/user/decorators.py
@@ -1,6 +1,6 @@
-from flask import session
+from flask import session, request
 from functools import wraps
-from src.exceptions import UnauthorizedUser
+from src.exceptions import UnauthorizedUser, InvalidUserField
 
 
 def role_required(role_id):
@@ -15,3 +15,17 @@ def role_required(role_id):
         return wrapper_func
 
     return decorator_func
+
+
+def validate_create_user_fields(view_func):
+    def wrapper_func(*args, **kwargs):
+        fields = request.get_json()
+        if (len(fields["name"]) < 3):
+            raise InvalidUserField(fields["name"])
+        if (len(fields["password"]) < 3):
+            raise InvalidUserField(fields["password"])
+        if (type(fields["role_id"]) != int):
+            raise InvalidUserField(fields["role_id"])
+        return view_func(*args, **kwargs)
+
+    return wrapper_func
