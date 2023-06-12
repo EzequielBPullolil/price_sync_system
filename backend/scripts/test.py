79ed401ebf50c6850416ce43ff2ec44da6e3056e
EzequielK-source <programacionezequielk@gmail.com>
2023-06-12 20:54:23 -0300

    Created SECRET_KEY env variable and added to flask instance

--- backend/scripts/test.py
+++ backend/scripts/test.py
@@ -5,6 +5,7 @@ import sys
 # Establecer las variables de entorno
 os.environ['PYTHONPATH'] = '.'
 os.environ['DATABASE_URI'] = 'postgresql://usertestservices:testuser@localhost/stock_sync_system_test'
+os.environ['SECRET_KEY'] = '2020-2020'
 
 # Borrar la pantalla (Windows: cls, Linux: clear)
 clear_command = 'cls' if os.name == 'nt' else 'clear'
--- backend/src/app.py
+++ backend/src/app.py
@@ -2,11 +2,12 @@ from flask import Flask, make_response, jsonify, session
 from src.inventory.routes import inventory_bp
 from src.user.routes import user_bp, auth_bp
 from src.exceptions import ApplicationLayerException, DomainException, UnauthorizedUser
+import os
 
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'my-secret-key'
+    app.secret_key = os.environ["SECRET_KEY"]
     app.register_blueprint(inventory_bp)
     app.register_blueprint(user_bp)
     app.register_blueprint(auth_bp)
