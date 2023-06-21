f5dd467ce138d002d8596fc196c5b2b7598ceed8
EzequielK-source <programacionezequielk@gmail.com>
2023-06-21 13:59:02 -0300

    Added dev script for run flask app

--- backend/scripts/dev.py
+++ backend/scripts/dev.py
@@ -4,15 +4,14 @@ import sys
 
 # Establecer las variables de entorno
 os.environ['PYTHONPATH'] = '.'
-os.environ['DATABASE_URI'] = 'postgresql://usertestservices:testuser@localhost/stock_sync_system_test'
+os.environ['DATABASE_URI'] = 'postgresql://usertestservices:testuser@localhost/stock_sync_system_dev'
 os.environ['SECRET_KEY'] = '2020-2020'
 
 # Borrar la pantalla (Windows: cls, Linux: clear)
 clear_command = 'cls' if os.name == 'nt' else 'clear'
 subprocess.call(clear_command, shell=True)
 
-# Ejecutar los tests con pytest
-pytest_command = ['pytest', '-s']
+flask_command = ['flask', '--app', 'src/app.py', 'run']
 # Pasa los argumentos adicionales al comando pytest
-pytest_command.extend(sys.argv[1:])
-subprocess.call(pytest_command)
+flask_command.extend(sys.argv[1:])
+subprocess.call(flask_command)
